6d08064c3d40e356a5f0f1ed5d2184d2bdf6aa3a
rami-lopez <ramiro.lopez@correo.ucu.edu.uy>
2024-11-06 11:41:26 -0300

    Tipos de pokemon mas avanzados

--- src/Library/Clases/Type.cs
+++ src/Library/Clases/Type.cs
@@ -4,26 +4,43 @@ public class Type
 {
     public enum PokemonType
     {
-        Agua,
-        Fuego,
-        Electrico,
-        Roca
+        Agua, Bicho, Dragon, Electrico, Fantasma, Fuego, Hielo, Lucha, Normal, Planta, Psiquico, Roca, Tierra, Veneno, Volador
     }
     
     public static int VentajaPokemon(PokemonType tipo1, PokemonType tipo2)
     {
         if (tipo1 == PokemonType.Agua && tipo2 == PokemonType.Fuego)
             return 1; 
-        
         if (tipo1 == PokemonType.Fuego && tipo2 == PokemonType.Roca)
             return 1; 
-        
         if (tipo1 == PokemonType.Electrico && tipo2 == PokemonType.Agua)
             return 1; 
-        
         if (tipo1 == PokemonType.Roca && tipo2 == PokemonType.Electrico)
             return 1;
-
+        if ( tipo1 == PokemonType.Planta && tipo2 == PokemonType.Agua)
+            return 1;
+        if (tipo1 == PokemonType.Agua && tipo2 == PokemonType.Planta)
+            return 1;
+        if (tipo1 == PokemonType.Planta && tipo2 == PokemonType.Roca)
+            return 1;
+        if (tipo1 == PokemonType.Roca && tipo2 == PokemonType.Planta)
+            return 1;
+        if (tipo1 == PokemonType.Planta && tipo2 == PokemonType.Veneno)
+            return 1;
+        if (tipo1 == PokemonType.Veneno && tipo2 == PokemonType.Planta)
+            return 1;
+        if (tipo1 == PokemonType.Fuego && tipo2 == PokemonType.Planta)
+            return 1;
+        if (tipo1 == PokemonType.Planta && tipo2 == PokemonType.Fuego)
+            return 1;
+        if (tipo1 == PokemonType.Planta && tipo2 == PokemonType.Volador)
+            return 1;
+        if (tipo1 == PokemonType.Volador && tipo2 == PokemonType.Planta)
+            return 1;
+        if (tipo1 == PokemonType.Volador && tipo2 == PokemonType.Bicho)
+            return 1;
+        if (tipo1 == PokemonType.Bicho && tipo2 == PokemonType.Volador)
+            return 1;
         if (tipo1 == tipo2)
             return 0; //empate
 
