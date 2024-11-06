c514381de9be2944821b47344166a72b2c4b4c03
Mate-Cardenas <mateo.cardenas@correo.ucu.edu.uy>
2024-11-06 12:17:29 -0300

    Update de Types
    Se hace refactor de los types a ingles

--- src/Library/Clases/Type.cs
+++ src/Library/Clases/Type.cs
@@ -5,29 +5,61 @@ public static class Type
     public static PokemonType type;
     public enum PokemonType
     {
+
         Water,
-        Fire,
+        Bug,
+        Dragon,
         Electric,
-        Plant
+        Ghost,
+        Fire,
+        Ice,
+        Fighter,
+        Normal,
+        Plant,
+        Psychic,
+        Rock,
+        Earth,
+        Poison,
+        Flying
     }
     
-    public static int VentajaPokemon(PokemonType type1, PokemonType type2)
+    public static int PokemonAdvantage(PokemonType type1, PokemonType type2)
     {
-        if (type1 == PokemonType.Water && type2 == PokemonType.Fire)
-            return 1; 
-        
-        if (type1 == PokemonType.Fire && type2 == PokemonType.Plant)
-            return 1; 
-        
-        if (type1 == PokemonType.Electric && type2 == PokemonType.Water)
-            return 1; 
-        
-        if (type1 == PokemonType.Plant && type2 == PokemonType.Electric)
+        if (type1 == PokemonType.Water && (type2 == PokemonType.Fire || type2 == PokemonType.Rock || type2 == PokemonType.Earth))
             return 1;
-
+        if (type1 == PokemonType.Bug && (type2 == PokemonType.Plant || type2 == PokemonType.Psychic))
+            return 1;
+        if (type1 == PokemonType.Dragon && type2 == PokemonType.Dragon)
+            return 1;
+        if (type1 == PokemonType.Electric && (type2 == PokemonType.Water || type2 == PokemonType.Flying))
+            return 1;
+        if (type1 == PokemonType.Ghost && (type2 == PokemonType.Ghost || type2 == PokemonType.Psychic))
+            return 1;
+        if (type1 == PokemonType.Fire && (type2 == PokemonType.Plant || type2 == PokemonType.Bug || type2 == PokemonType.Ice))
+            return 1;
+        if (type1 == PokemonType.Ice && (type2 == PokemonType.Plant || type2 == PokemonType.Earth || type2 == PokemonType.Flying || type2 == PokemonType.Dragon))
+            return 1;
+        if (type1 == PokemonType.Fighter && (type2 == PokemonType.Normal || type2 == PokemonType.Rock || type2 == PokemonType.Ice))
+            return 1;
+        if (type1 == PokemonType.Plant && (type2 == PokemonType.Water || type2 == PokemonType.Earth || type2 == PokemonType.Rock))
+            return 1;
+        if (type1 == PokemonType.Psychic && type2 == PokemonType.Fighter)
+            return 1;
+        if (type1 == PokemonType.Rock && (type2 == PokemonType.Fire || type2 == PokemonType.Flying || type2 == PokemonType.Ice || type2 == PokemonType.Bug))
+            return 1;
+        if (type1 == PokemonType.Earth && (type2 == PokemonType.Fire || type2 == PokemonType.Electric || type2 == PokemonType.Poison || type2 == PokemonType.Rock))
+            return 1;
+        if (type1 == PokemonType.Poison && type2 == PokemonType.Plant)
+            return 1;
+        if (type1 == PokemonType.Flying && (type2 == PokemonType.Plant || type2 == PokemonType.Fighter || type2 == PokemonType.Bug))
+            return 1;
+        
+        // Empate
         if (type1 == type2)
-            return 0; //empate
+            return 0; 
+        
+        // Desventaja en los demas casos
+        return -1; 
 
-        return -1; //desventaja en los demas
     }
 }
